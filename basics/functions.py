566fe8a05f97796347bec45fa40fc8c661b59871
HinnMachache <bennyhinn253.bh@gmail.com>
2023-09-19 19:02:06 +0300

    More Python Basic

--- basics/functions.py
+++ basics/functions.py
@@ -1,5 +1,4 @@
 def hello(name="World"):
-    name = input("Enter name: ")
     print(f"Hello {name}")
 
 
@@ -8,7 +7,67 @@ def square(num):
     return num_square
 
 
-num = int(input("Enter a number: "))
-print(square(num))
-name = input("Enter name: ")
-hello(name)
+def solve_forX(equation):
+    # Split elements into variables
+    x, add, num1, equals, num2 = equation.split()
+    # Cast to int
+    num1, num2 = int(num1), int(num2)
+    # Print Results
+    return "x = " + str(num2 - num1)
+
+
+# print(solve_forX("x + 8 = 11"))
+
+
+def fibonacci(num):
+    """Print a fibonacci series up to num."""
+    num1, num2 = 0, 1
+    while num1 < num:
+        print(num1, end=" ")
+        num1, num2 = num2, num1 + num2
+
+    print()
+
+
+def fibonacci_append(num):
+    """Return a list of fibonacci series up to num."""
+    fibonacci_list = []
+    num1, num2 = 0, 1
+    while num1 < num:
+        fibonacci_list.append(num1)
+        num1, num2 = num2, num1 + num2
+    return fibonacci_list
+
+    print()
+
+
+"""
+fibonacci_num = fibonacci_append(3)
+print(fibonacci_num)
+print(fibonacci(3))
+"""
+
+# Variable shadowing
+
+x = 20  # Global Variable
+
+
+def outerFunc():
+    global x
+    x = 30  # Variable shadowing
+    y = 10  # Variable Shadowing
+    print(x, y)
+
+    def innerFunc():
+        global x
+        nonlocal y
+        x = 10
+        y = 49
+        print(x, y)
+
+    innerFunc()
+    print(x , y)
+
+
+outerFunc()
+print(x)
